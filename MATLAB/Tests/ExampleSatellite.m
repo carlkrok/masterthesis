ae287821fec15884a60fc688bbaeb1c6a9f1759e
carlkrok <carlmk@stud.ntnu.no>
2020-06-04 17:09:40 +0200

    Added simulation

--- MATLAB/Tests/ExampleSatellite.m
+++ MATLAB/Tests/ExampleSatellite.m
@@ -17,10 +17,10 @@ simConfig.enableRW = true;
 simConfig.enableMTQ = true;
 %simConfig.enableMTQ = false;
 
-simConfig.enablePropulsion = true;
-%simConfig.enablePropulsion = false;
-simConfig.enablePropulsionInertia = true;
-%simConfig.enablePropulsionInertia = false;
+%simConfig.enablePropulsion = true;
+simConfig.enablePropulsion = false;
+%simConfig.enablePropulsionInertia = true;
+simConfig.enablePropulsionInertia = false;
 
 eulerFirst = pi/3;
 eulerSecond = pi/8;
@@ -89,7 +89,7 @@ sat_period = SatellitePeriod( MU_EARTH, sat.initCond.orb_a );
 timestep_pd = 0.1;
 timestep_controller = 1; % 2; 
 timestep_prediction = 1; % 2; 
-prediction_horizon = 4;% 10; % 10 
+prediction_horizon = 5;% 10; % 10 
 duration = 150; %numSteps*stepLength;
 eph = SimulateSatellite_integerMPC( t0_MJD, satelliteFilename, timestep_controller, ...
     timestep_prediction, duration, prediction_horizon, numControlVariables, ...
